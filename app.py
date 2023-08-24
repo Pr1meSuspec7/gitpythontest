d381b945e8d1aa9aeb09c38e687e05b44b945e3a
Administrator <admin@example.com>
2023-08-24 18:20:29 +0200

    backup 2023-08-24@18:20:29

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from git import Repo
+from datetime import datetime
+
+now = datetime.now().isoformat("@", "seconds")
+commit_message = "backup " + str(now)
+repo_token = "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"
+repo_folder = "."
+repo = Repo(repo_folder)
+repo_url = repo.remotes.origin.url.split('https://')[1]
+repo_token_url = "https://" + repo_token + "@" + repo_url
+
+#repo.git.add(".")
+#repo.git.commit(m=commit_message)
+#repo.git.push(repo_token_url)
+
+
+def git_push(local_repo, msg, repourl_w_token):
+    try:
+        local_repo.git.add(".")
+        local_repo.git.commit(m=msg)
+        local_repo.git.push(repourl_w_token)
+    except:
+        print('Some error occured while pushing the code')    
+
+git_push(repo, commit_message, repo_token_url)
